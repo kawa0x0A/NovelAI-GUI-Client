20c66e69af5a774032a69d83e4c524c2e7186618
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2026-06-07 17:21:41 +0900

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,19 @@
-﻿namespace NovelAI_GUI_Client
+namespace NovelAI_GUI_Client
 {
     public partial class App : Application
     {
+        private readonly IHttpClientFactory httpClientFactory;
+
         public App(IHttpClientFactory httpClientFactory)
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage(httpClientFactory));
+            this.httpClientFactory = httpClientFactory;
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new NavigationPage(new MainPage(httpClientFactory)));
         }
     }
 }
